944b36456af543386446134528a1cd93fa1b94a2
aflat <ghstock@gmail.com>
2015-03-08 13:06:53 -0400

    this creates an instance, but no public IP yet

--- newwww.py
+++ newwww.py
@@ -9,7 +9,9 @@ import pexpect
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/boto")
 import boto.ec2
+from boto.vpc import VPCConnection
 
+import os.path
 import string
 import subprocess
 import logging
@@ -97,36 +99,103 @@ def RunSalt(instanceAddress,instanceUser, instancePassword):
 	child.expect ('.*]#',timeout=210)
 	child.sendline ('exit')
 
-#################################################################################################
-##
-##	function: InitAWS
-##	purpose: initializes a aws connection
-##	parameters: instanceAddress - the address of the instance we are going to configure
-##				instanceUser - the user of the instance we are gong to configure
-##				instancePassword - the password of the instance we are gong to configure
-##	returns: 
-##
-#################################################################################################
-def InitAWS():
-	logging.debug( "initting aws")
+class AWS(object):
+	credsFilename = "aws.creds"
+	awsID = ""
+	awsSecretKey = ""
+	ec2 = None
+	vpcConnection = None
+	vpc = None
+	subnet = None
+	subnetAddressRange = "10.10.0.0/24"
+	
+	def __init__(self, credsFilename):
+		config = ConfigParser.RawConfigParser()
+		config.read(credsFilename)
+		self.awsID = config.get('CredentialsSection', 'AWS_ACCESS_KEY_ID')
+		self.awsSecretKey = config.get('CredentialsSection', 'AWS_SECRET_ACCESS_KEY')
 
-#################################################################################################
-##
-##	function: GetAWSCreds
-##	purpose: gets the aws credentials from the aws.creds file
-##	parameters: credsFilename - the name of the file to read the creds from
-##	returns: 
-##
-#################################################################################################
-def GetAWSCreds(credsFilename):
-	config = ConfigParser.RawConfigParser()
-	config.read(credsFilename)
-	awsID = config.get('CredentialsSection', 'AWS_ACCESS_KEY_ID')
-	awsSecretKey = config.get('CredentialsSection', 'AWS_SECRET_ACCESS_KEY')
+		
+		logging.debug("got creds file:" + credsFilename)
+		logging.debug("awsID:" + self.awsID)
+		logging.debug("awsSecret:" + self.awsSecretKey)
+		if( len(self.awsID) == 0 or len(self.awsSecretKey) == 0):
+			logging.error("ERROR: You must provide your AWS crendentials in the file " + credsFilename)
+			sys.exit(1)
 
-	logging.debug("got creds file:" + credsFilename)
-	logging.debug("awsID:" + awsID)
-	logging.debug("awsSecret:" + awsSecretKey)
+		self.ec2 = boto.connect_ec2(aws_access_key_id=self.awsID, aws_secret_access_key=self.awsSecretKey)
+		if(self.ec2 == None):
+			logging.error("ERROR: Failed to connect to the ec2 instance with the given creds in file: " + credsFilename)
+			sys.exit(1)	
+
+
+	#################################################################################################
+	##
+	##	function: CreateVPC
+	##	purpose: creates the vpc
+	##	parameters: 
+	##	returns: none
+	##
+	#################################################################################################
+	def CreateVPC(self):
+		if(self.vpcConnection == None):
+			self.vpcConnection = VPCConnection(aws_access_key_id=self.awsID, aws_secret_access_key=self.awsSecretKey)
+		self.vpc = self.vpcConnection.create_vpc(self.subnetAddressRange)
+	#################################################################################################
+	##
+	##	function: CreateSubnet
+	##	purpose: creates a subnet
+	##	parameters: 
+	##	returns: the subnet object
+	##
+	#################################################################################################
+	def CreateSubnet(self):
+		if(self.vpcConnection == None):
+			self.vpcConnection = VPCConnection(aws_access_key_id=self.awsID, aws_secret_access_key=self.awsSecretKey)
+		if(self.vpc == None):
+			self.CreateVPC()
+		self.subnet = self.vpcConnection.create_subnet(self.vpc.id, self.subnetAddressRange)
+
+	#################################################################################################
+	##
+	##	function: RunInstance
+	##	purpose: initializes a aws connection
+	##	parameters: 
+	##	returns: 
+	##
+	#################################################################################################
+	def RunInstance(self):
+		ec2KeyName = "ec2-newwww-key"
+		#amiID = "ami-96a818fe" #this is centos7, but comes from the marketplace, 
+								#so if you haven't agreed to the marketplace agreement, you can't launch it
+		amiID = "ami-48400720" #redhat ami id
+		if(self.subnet == None):
+			self.CreateSubnet()
+		
+		if( not os.path.isfile(ec2KeyName + ".pem")): 
+			logging.debug("need to create the the keypair: " + ec2KeyName)
+			key_pair = ec2.create_key_pair(ec2KeyName)
+			key_pair.save('./')
+		#print self.ec2.run_instances.__doc__
+		
+		reservation = self.ec2.run_instances(image_id=amiID, 
+										key_name=ec2KeyName, 
+										instance_type="t2.micro",
+										#security_group_ids=['sg-dfa62ebb'],
+										subnet_id=self.subnet.id
+										)
+	#################################################################################################
+	##
+	##	function: GetCreds
+	##	purpose: gets the aws credentials from the aws.creds file
+	##	parameters: none
+	##	returns: a hashmap of keyID,secretKey
+	##
+	#################################################################################################
+	def GetCreds(self):
+		return awsID,awsSecretKey
+
+
 
 
 parser = optparse.OptionParser(description='Create a www server in AWS')
@@ -135,8 +204,11 @@ parser.add_option('--creds', '--c' ,  dest='credsFile',default="aws.creds",
 
 options, args = parser.parse_args()
 
-
-GetAWSCreds(options.credsFile)
+aws = AWS(options.credsFile)
+aws.RunInstance()
+#aws.RunInstance()
+#awsID,awsSecretKey = GetAWSCreds(options.credsFile)
+#RunAWSInstance(awsID, awsSecretKey)
 #InstallSalt("192.168.1.15","root","mypassword")
 #ConfigureSalt("192.168.1.15","root","mypassword")
 #RunSalt("192.168.1.15","root","mypassword")
